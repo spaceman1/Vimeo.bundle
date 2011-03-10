520ee257722920f6c883f2c4532e31f53d5e2c58
James Clarke <james@jc85.net>
2011-03-09 21:43:29 -1000

    Updated to use the URL service and new object classes.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -8,16 +8,15 @@ VIMEO_LOAD_CLIP   = 'http://www.vimeo.com/moogaloop/load/clip:%s/local?param_md5
 VIMEO_PLAY_CLIP   = 'http://www.vimeo.com/moogaloop/play/clip:%s/%s/%s/?q=%s&type=local'
 VIMEO_DIRECTORY   = 'http://vimeo.com/%s/%s/page:%d'
 VIMEO_SEARCH      = 'http://vimeo.com/search/videos/search:%s/%s/page:%d/sort:plays/format:detail'
-CLIENT_CAP_HEADER = 'X-Plex-Client-Capabilities'
 
 ####################################################################################################
 def Start():
   Plugin.AddPrefixHandler(VIMEO_PREFIX, MainMenu, 'Vimeo', 'icon-default.jpg', 'art-default.png')
   Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
-  MediaContainer.title1 = 'Vimeo'
-  MediaContainer.content = 'Items'
-  MediaContainer.art = R('art-default.png')
-  DirectoryItem.thumb = R('icon-default.jpg')
+  ObjectContainer.title1 = 'Vimeo'
+  ObjectContainer.content = ContainerContent.GenericVideos
+  ObjectContainer.art = R('art-default.png')
+  DirectoryObject.thumb = R('icon-default.jpg')
   HTTP.CacheTime = CACHE_INTERVAL
 
 ####################################################################################################
@@ -28,21 +27,55 @@ def UpdateCache():
 
 ####################################################################################################
 def MainMenu():
-  dir = MediaContainer()
-  dir.Append(Function(DirectoryItem(GetMyStuff,       title=L("My Stuff"))))
-  dir.Append(Function(DirectoryItem(GetVideosRSS,     title="Staff Picks", thumb=R('staffpicks.png')), name='channels/staffpicks/videos', title2='Staff Picks'))
-  dir.Append(Function(DirectoryItem(FeaturedChannels, title="Featured Channels", thumb=R('featured.png'))))
-  dir.Append(Function(DirectoryItem(GetVideosRSS,     title="High Def", thumb=R('hd.png')), name='channels/hd/videos', title2="High Def"))
-  dir.Append(Function(DirectoryItem(Categories,       title="Channels", thumb=R('channels.png')), noun='channels', url='all'))
-  dir.Append(Function(DirectoryItem(Categories,       title=L("Groups"), thumb=R('groups.png')), noun='groups', url='all', sort='members'))
-  dir.Append(Function(SearchDirectoryItem(Search,     title=L("Search"), prompt=L("Search for Videos"), thumb=R('search.png'))))
-  dir.Append(PrefsItem(L("Preferences..."), thumb=R('prefs.png')))
+  oc = ObjectContainer(
+    objects = [
+      DirectoryObject(
+        key     = Callback(GetMyStuff),
+        title   = L('My Stuff')
+      ),
+      DirectoryObject(
+        key     = Callback(GetVideosRSS, name='channels/staffpicks/videos', title2='Staff Picks'),
+        title   = L('Staff Picks'),
+        thumb   = R('staffpicks.png')
+      ),
+      DirectoryObject(
+        key     = Callback(FeaturedChannels),
+        title   = L('Featured Channels'),
+        thumb   = R('featured.png')
+      ),
+      DirectoryObject(
+        key     = Callback(GetVideosRSS, name='channels/hd/videos', title2='High Def'),
+        title   = L('High Def'), 
+        thumb   = R('hd.png')
+      ),
+      DirectoryObject(
+        key     = Callback(Categories, noun='channels', url='all'),
+        title   = L('Channels'), 
+        thumb   = R('channels.png')
+      ),
+      DirectoryObject(
+        key     = Callback(Categories, noun='groups', url='all', sort='members'),
+        title   = L('Groups'), 
+        thumb   = R('groups.png')
+      ),
+      InputDirectoryObject(
+        key     = Callback(Search),
+        title   = L('Search'),
+        prompt  = L('Search for Videos'),
+        thumb   = S('search.png')
+      ),
+      PrefsObject(
+        title   = L('Preferences...'),
+        thumb   = R('prefs.png')
+      )
+    ]
+  )
   
-  return dir
+  return oc
 
 ####################################################################################################
-def GetMyStuff(sender):
-  dir = MediaContainer()
+def GetMyStuff():
+  oc = ObjectContainer()
 
   # See if we need to log in.
   xml = HTML.ElementFromURL(VIMEO_URL + 'subscriptions/channels/sort:name', cacheTime=0)
@@ -53,6 +86,7 @@ def GetMyStuff(sender):
     # Try to log in
     Login()
     Log(xml.xpath('//title')[0].text)
+    
     # Now check to see if we're logged in.
     xml = HTML.ElementFromURL(VIMEO_URL + 'subscriptions/channels/sort:name', cacheTime=0)
     if xml.xpath('//title')[0].text != 'Your subscriptions on Vimeo':
@@ -64,26 +98,45 @@ def GetMyStuff(sender):
       url = item.get('href')
       Log(url)
       junk, noun, link = url.split('/')
-      title = item.text# + item.find('span').text
+      title = item.text
+      
       if item.text.strip() == 'My Likes':
         userUrl = name=item.get('href')[1:]
-        dir.Append(Function(DirectoryItem(GetVideosRSS, title), name=userUrl, title2="My Likes"))
+        oc.add(DirectoryObject(
+          key = Callback(GetVideosRSS, name=userUrl, title2="My Likes"),
+          title = title
+        ))
       elif item.text.strip() == 'My Groups':
-        dir.Append(Function(DirectoryItem(GetDirectory, title), noun=noun, url=link, sort='name', narrow='joined'))
+        oc.add(DirectoryObject(
+          key = Callback(GetDirectory, noun=noun, url=link, sort='name', narrow='joined'),
+          title = title
+        ))
       elif item.text.strip() == 'My Channels':
-        dir.Append(Function(DirectoryItem(GetDirectory, title), noun=noun, url=link, sort='name', narrow='subscribe'))
+        oc.add(DirectoryObject(
+          key = Callback(GetDirectory, noun=noun, url=link, sort='name', narrow='subscribe'),
+          title = title
+        ))
       elif item.text.strip() == 'My Videos':
-        dir.Append(Function(DirectoryItem(GetVideosRSS, title), name=item.get('href')[1:], title2='My Videos'))
-        dir.Append(Function(DirectoryItem(GetContacts, "My Contacts"), url=item.get('href').replace('videos', 'contacts')))
+        oc.add(DirectoryObject(
+          key = Callback(GetVideosRSS, name=item.get('href')[1:], title2='My Videos'),
+          title = title
+        ))
+        oc.add(DirectoryObject(
+          key = Callback(GetContacts, url=item.get('href', title2=title2).replace('videos', 'contacts'), title2='My Videos'),
+          title = title
+        ))
     
-  dir.Append(Function(DirectoryItem(GetVideosRSS, 'My Subscriptions'), name=userUrl.replace('likes','subscriptions/videos'), title2='My Subscriptions'))
+  oc.add(DirectoryObject(
+    key = Callback(GetVideosRSS, name=userUrl.replace('likes','subscriptions/videos'), title2='My Subscriptions'),
+    title = L('My Subscriptions'),
+  ))
 
   return dir
 
 ####################################################################################################
-def GetContacts(sender, url):
-  dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
-
+def GetContacts(url, title2=None):
+  oc = ObjectContainer(title2=title2, view_group='Details')
+  
   url += '/sort:name'
   for contact in HTML.ElementFromURL(VIMEO_URL + url).xpath('//div[@class="contact"]'):
     thumb = contact.find('img').get('src')
@@ -98,45 +151,62 @@ def GetContacts(sender, url):
 
     summary = '\n'
     try:
-        summary += info.xpath('a[@class="contacts"]')[0].text + ", "
+      summary += info.xpath('a[@class="contacts"]')[0].text + ", "
     except:
-        pass
+      pass
 
     try:
-        summary += info.xpath('a[@class="videos"]')[0].text
-        url = info.xpath('a[@class="videos"]')[0].get('href')
-        dir.Append(Function(DirectoryItem(GetVideosRSS, title=title, subtitle=subtitle, thumb=thumb, summary=summary), name=url, title2=title))
+      summary += info.xpath('a[@class="videos"]')[0].text
+      url = info.xpath('a[@class="videos"]')[0].get('href')
+      oc.add(DirectoryObject(
+        key = Callback(GetVideosRSS, name=url, title2=title),
+        title = title,
+        tagline = subtitle,
+        thumb = thumb,
+        summary = summary,
+      ))
     except:
-        # doesn't have any videos for some reason.. skip
-        pass
+      # doesn't have any videos for some reason.. skip
+      pass
 
-  return dir
+  return oc
 
 ####################################################################################################
-def FeaturedChannels(sender):
-  dir = MediaContainer(title2='Featured Channels')
+def FeaturedChannels():
+  oc = ObjectContainer(title2='Featured Channels')
   for c in HTML.ElementFromURL(VIMEO_URL + 'channels').xpath("//div[@class='badge']"):
     title = c.find('a').get('title')
     thumb = re.findall("'(.*)'", c.get('style'))[0]
     url = c.find('a').get('href')
     url = url[url.rfind('/')+1:]
-    dir.Append(Function(DirectoryItem(GetVideosRSS, title, thumb=thumb), name='channels/' + url + '/videos', title2=title))
-  return dir
+    oc.add(DirectoryObject(
+      key = Callback(GetVideosRSS, name='channels/' + url + '/videos', title2=title),
+      title = title,
+      thumb = thumb
+    ))
+
+  return oc
 
 ####################################################################################################
-def Categories(sender, noun, url, sort='subscribed'):
-  dir = MediaContainer(viewGroup='Details', title2='Channels')
+def Categories(noun, url, sort='subscribed'):
+  oc = ObjectContainer(title2='Channels', view_group='Details')
   for category in HTML.ElementFromURL(VIMEO_URL + 'channels').xpath('//div[@id="cloud"]/ul/li'):
     title = string.capwords(category.find('a').text)
     subtitle = category.find('span').text + ' ' + noun
     cat = category.find('a').get('href')
     cat = cat[cat.find(':')+1:]
-    dir.Append(Function(DirectoryItem(GetDirectory, title, subtitle=subtitle), category=cat, noun=noun, url=url, sort=sort))
-  return dir
+
+    oc.add(DirectoryObject(
+      key = Callback(GetDirectory, category=cat, noun=noun, url=url, sort=sort, title2=title),
+      title = title,
+      tagline = subtitle,
+    ))
+
+  return oc
 
 ####################################################################################################
-def GetDirectory(sender, category=None, noun=None, url=None, page=1, sort='subscribed', narrow=None):
-  dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle, replaceParent=(page>1))
+def GetDirectory(category=None, noun=None, url=None, page=1, sort='subscribed', narrow=None, title2=None):
+  oc = ObjectContainer(title2=title2, view_group='Details', replace_parent=(page > 1))
 
   the_url = VIMEO_DIRECTORY % (noun, url, page)
   if category is not None:
@@ -180,21 +250,33 @@ def GetDirectory(sender, category=None, noun=None, url=None, page=1, sort='subsc
     thumb = link.find('img').get('src')
     channel = link.get('href')
     channel = channel[channel.rfind('/')+1:]
-    dir.Append(Function(DirectoryItem(GetVideosRSS, title=title, summary=desc, subtitle=subtitle, thumb=thumb), name=noun+'/'+channel+'/videos', title2=title))
+    
+    oc.add(DirectoryObject(
+      key = Callback(GetVideosRSS, name=noun+'/'+channel+'/videos', title2=title),
+      title = title,
+      tagline = subtitle,
+      summary = desc,
+      thumb = thumb 
+    ))
 
-  dir.Append(Function(DirectoryItem(GetDirectory, title="More..."), category=category, noun=noun, url=url, sort=sort, narrow=narrow, page=page+1))
-  return dir
+  oc.add(DirectoryObject(
+    key = Callback(GetDirectory, category=category, noun=noun, url=url, sort=sort, narrow=narrow, page=page+1, title2=title2),
+    title = L('More...'),
+  ))
+
+  return oc
 
 ####################################################################################################
-def Search(sender, query, page=1):
-  dir = MediaContainer(viewGroup='Details', title2='Search Results', replaceParent=(page>1))
+def Search(query, page=1):
+  oc = ObjectContainer(title2=L('Search Results'), view_group='details', replace_parent=(page > 1))
   query = query.replace(' ', '+')
   
   # Need to get the security token.
-  vimeo_page = HTML.ElementFromURL(VIMEO_URL)
+  vimeo_page = HTML.ElementFromURL(VIMEO_URL, cacheTime=0)
   security_token = vimeo_page.xpath('//input[@id="xsrft"]')[0].get('value')[0:8]
   
-  for result in HTML.ElementFromURL(VIMEO_SEARCH % (query, security_token, page), headers={"Cookie" : "searchtoken="+security_token}).xpath('//div[@class="item last"]'):
+  vimeo_html = HTTP.Request(VIMEO_SEARCH % (query, security_token, page), headers={"Cookie" : "searchtoken="+security_token}).content
+  for result in HTML.ElementFromString(vimeo_html).xpath('//div[@class="item last"]'):
     title = result.xpath('div/div[@class="title"]/a')[0].text
     subtitle_items = [e.strip() for e in result.xpath('div/div[@class="date"]')[0].itertext()]
     subtitle = "%s (%s plays)" % (subtitle_items[0], subtitle_items[2])
@@ -203,45 +285,34 @@ def Search(sender, query, page=1):
     try:
         link = result.xpath('div[@class="thumbnail_box"]/a[@class="thumbnail"]')[0]
         thumb = link.find('img').get('src')
-        key = link.get('href')[1:]
+        url = 'http://vimeo.com' + link.get('href')
     except:
         PMS.Log(XML.StringFromElement(result))
         continue
-    dir.Append(Function(VideoItem(PlayVideo, title, subtitle, desc, thumb=thumb), ext='flv', id=key))
+    #
+    oc.add(VideoClipObject(
+      title = title,
+      tagline = subtitle,
+      summary = desc,
+      thumb = thumb,
+      url = url
+    ))
+    
+  oc.add(DirectoryObject(
+    key = Callback(Search, query=query, page=page+1),
+    title = L('More...')
+  ))
+  return oc
 
-  dir.Append(Function(DirectoryItem(Search, title="More..."), query=query, page=page+1))
-  return dir
-
-def StripTags(str):
-  return re.sub(r'<[^<>]+>', '', str)
 
 ####################################################################################################
-def GetVideosRSS(sender, name, title2):
+def GetVideosRSS(name, title2):
   cookies = HTTP.GetCookiesForURL(VIMEO_URL)
 
-  direct = False
-  direct_high = False
-
-  # Move this somewhere common - unless I missed it.
-  capabilities = {}
-  capabilities['protocols'] = []
-  if CLIENT_CAP_HEADER in Request.Headers:
-    elements = Request.Headers[CLIENT_CAP_HEADER].split(';')
-    for el in elements:
-      name_values = el.split('=')
-      values = name_values[1].split(',')
-      capabilities[name_values[0]] = values
-      
-  # Add a directKey="" which returns an error or a URL (Vimeo mobile videos don't always exist).
-  if 'http-streaming-video-720p' in capabilities['protocols']:
-    direct = True
-    direct_high = True
-    print "High Resolution Direct"
-  elif 'http-streaming-video' in capabilities['protocols']:
-    direct = True
-    print "Direct"
+  #dir = MediaContainer(viewGroup='Details', title2=title2, httpCookies=cookies)
+  #TODO: ViewGroup
+  oc = ObjectContainer(title2=title2, http_cookies=cookies, view_group='Details')
   
-  dir = MediaContainer(viewGroup='Details', title2=title2, httpCookies=cookies)
   for video in XML.ElementFromURL(VIMEO_URL + name + '/rss', errors="ignore").xpath('//item', namespaces=VIMEO_NAMESPACE):
     title = video.find('title').text
     date = Datetime.ParseDate(video.find('pubDate').text).strftime('%a %b %d, %Y')
@@ -250,96 +321,20 @@ def GetVideosRSS(sender, name, title2):
       thumb = video.xpath('m:content/m:thumbnail', namespaces=VIMEO_NAMESPACE)[0].get('url')
       key = video.xpath('m:content/m:player', namespaces=VIMEO_NAMESPACE)[0].get('url')
       key = key[key.rfind('=')+1:]
-      directKey = Function(GetDirectVideo, id=key, high=direct_high)
-      directKey = None
-      summary = StripTags(video.find('description').text)
-      dir.Append(Function(VideoItem(PlayVideo, title, date, summary, thumb=thumb, keyDirect=directKey), id=key))
+      summary = String.StripTags(video.find('description').text)
+      
+      url = 'http://vimeo.com/%s' % key
+      
+      oc.add(VideoClipObject(
+        title = title,
+        tagline = date,
+        summary = summary,
+        thumb = thumb,
+        url = url
+      ))
     except:
       pass
-  return dir
-
-import urllib2, httplib
-class SmartRedirectHandler(urllib2.HTTPRedirectHandler):     
-    def http_error_301(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPRedirectHandler.http_error_301( 
-            self, req, fp, code, msg, headers)              
-        result.status = code                                 
-        return result                                       
-
-    def http_error_302(self, req, fp, code, msg, headers):   
-        result = urllib2.HTTPRedirectHandler.http_error_302(
-            self, req, fp, code, msg, headers)              
-        result.status = code                                
-        return result
-
-def GetMediaUrl(id, hd=True):
-  headers = {'User-agent': 'AppleCoreMedia/1.0.0.10F569 (iPad; U; CPU OS 10_6_4 like Mac OS X)'}
-  url = "http://www.vimeo.com/play_redirect?quality=%s&codecs=H264&clip_id=%s" % ('hd' if hd else 'sd', id)
-  
-  try:
-    print "Trying:", url
-    request = urllib2.Request(url, None, headers)
-    opener = urllib2.build_opener(SmartRedirectHandler)
-    f = opener.open(request)
-    if f.status == 301 or f.status == 302:
-      return f.url
-  except:
-    pass
-    
-  return None
-
-####################################################################################################
-def GetDirectVideo(id, high, sender=None):
-
-  dir = MediaContainer()
-  dir.art = dir.content = dir.noHistory = dir.replaceParent = dir.title1 = None
-
-  #
-  # iPad HD video:
-  #    $ curl --verbose "http://www.vimeo.com/play_redirect?quality=hd&codecs=H264&clip_id=13576492" --header "User-Agent: AppleCoreMedia/1.0.0.10F569 (iPad; U; CPU OS 10_6_4 like Mac OS X)"
-  #
-  # iPhone low-resolution video:
-  #
-  # Tries direct stream - fails.
-  # Needs to transcode - on which stream???
-  #    <Media direct="1" key="...." />
-  #    <Media key="" />
-  #
-  #    $ curl --verbose "http://www.vimeo.com/play_redirect?quality=mobile&clip_id=13576492" --header "Referer: http://www.vimeo.com/m/"
-  #
-  url = GetMediaUrl(id, high)
-  if not url and high == True:
-    url = GetMediaUrl(id, False)
-
-  if url:
-    print "Got URL:", url
-    dir.Append(VideoItem(url, None, None, None))
-
-  return dir
-
-####################################################################################################
-def PlayVideo(sender, id):
-  headers = {'User-agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_5; en-us) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5', 'Cookie' : HTTP.GetCookiesForURL(VIMEO_URL) }
-  video = HTTP.Request('http://www.vimeo.com/%s' % id, cacheTime=0, headers=headers).content
-
-  m1 = re.search('"hd":([0-9])', video)
-  m2 = re.search('"signature":"([0-9a-f]+)","timestamp":([0-9]+)', video)
-  
-  if m1 and m2:
-    hd = int(m1.groups()[0])
-    if Prefs['hd'] == True and hd == True:
-      format = 'hd'
-    else:
-      format = 'sd'
-      
-    (sig, time) = m2.groups()
-    headers['Referer'] = 'http://vimeo.com/%s' % id
-    url = 'http://player.vimeo.com/play_redirect?clip_id=%s&sig=%s&time=%s&quality=%s&codecs=H264,VP8,VP6&type=moogaloop_local&embed_location=' % (id, sig, time, format)
-    request = urllib2.Request(url, None, headers)
-    opener = urllib2.build_opener(SmartRedirectHandler)
-    f = opener.open(request)
-    if f.status == 301 or f.status == 302:
-      return Redirect(f.url)
+  return oc
 
 ####################################################################################################
 def Login():
